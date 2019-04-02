c68cdb0db2f7163d980b8ec6b378624e3756f6b2
Fred Bradley <hello@fredbradley.co.uk>
2019-04-02 14:08:30 +0100

    update so that when on the plugins page on wordpress it displays useful informaiton

--- cranleigh-admissions-portal-plugin.php
+++ cranleigh-admissions-portal-plugin.php
@@ -1,13 +1,13 @@
 <?php
 /**
 Plugin Name: Cranleigh Admissions Portal
-Plugin URI: https://github.com/cranleighschool/admissions-portal
-Description: Cranleigh Admissions Portal Plugin
-Version: 1.1
+Plugin URI: https://github.com/cranleighschool/cranleigh-admissions-portal-plugin
+Description: Cranleigh Admissions Portal Plugin. The thing that bridges the gap between our websites and our portal.
+Version: 1.2
 Author: fredbradley
 Author URI: https://www.cranleigh.org
 License: GPL2
- */
+*/
 
 namespace FredBradley\CranleighAdmissionsPlugin;
 
